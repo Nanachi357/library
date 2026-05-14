4849166fbfba95e54b731865bfd504c6a2007a6d
Nanachi357 <109426637+Nanachi357@users.noreply.github.com>
2026-05-14 19:47:44 +0300

    feat(mapper): add update and patch entity mapping

--- src/main/java/com/github/nanachi357/library/mapper/AuthorMapper.java
+++ src/main/java/com/github/nanachi357/library/mapper/AuthorMapper.java
@@ -1,10 +1,15 @@
 package com.github.nanachi357.library.mapper;
 
+import com.github.nanachi357.library.dto.PatchAuthorRequest;
 import com.github.nanachi357.library.dto.AuthorResponse;
 import com.github.nanachi357.library.dto.CreateAuthorRequest;
+import com.github.nanachi357.library.dto.UpdateAuthorRequest;
 import com.github.nanachi357.library.entity.Author;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface AuthorMapper {
@@ -13,5 +18,12 @@ public interface AuthorMapper {
 
     @Mapping(target = "id", ignore = true)
     Author toEntity(CreateAuthorRequest request);
+
+    @Mapping(target = "id", ignore = true)
+    void updateEntity(UpdateAuthorRequest request, @MappingTarget Author author);
+
+    @Mapping(target = "id", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void patchEntity(PatchAuthorRequest request, @MappingTarget Author author);
 
 }
--- src/main/java/com/github/nanachi357/library/mapper/BookMapper.java
+++ src/main/java/com/github/nanachi357/library/mapper/BookMapper.java
@@ -1,10 +1,15 @@
 package com.github.nanachi357.library.mapper;
 
+import com.github.nanachi357.library.dto.PatchBookRequest;
 import com.github.nanachi357.library.dto.BookResponse;
 import com.github.nanachi357.library.dto.CreateBookRequest;
+import com.github.nanachi357.library.dto.UpdateBookRequest;
 import com.github.nanachi357.library.entity.Book;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface BookMapper {
@@ -13,5 +18,12 @@ public interface BookMapper {
 
     @Mapping(target = "id", ignore = true)
     Book toEntity(CreateBookRequest request);
+
+    @Mapping(target = "id", ignore = true)
+    void updateEntity(UpdateBookRequest request, @MappingTarget Book book);
+
+    @Mapping(target = "id", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void patchEntity(PatchBookRequest request, @MappingTarget Book book);
 
 }
