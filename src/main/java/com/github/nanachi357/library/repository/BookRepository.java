fa285fa82d4cc9e0516cebe4f3eb143755e54429
Nanachi357 <109426637+Nanachi357@users.noreply.github.com>
2026-05-29 22:46:45 +0300

    feat(repository): add book relation queries

--- src/main/java/com/github/nanachi357/library/repository/BookRepository.java
+++ src/main/java/com/github/nanachi357/library/repository/BookRepository.java
@@ -2,6 +2,24 @@ package com.github.nanachi357.library.repository;
 
 import com.github.nanachi357.library.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query("""
+            select b from Book b
+            join b.authors a
+            where a.id = :authorId
+            """)
+    List<Book> findAllByAuthorId(@Param("authorId") Long authorId);
+
+    @Query("""
+            select b from Book b
+            join b.readers r
+            where r.id = :readerId
+            """)
+    List<Book> findAllByReaderId(@Param("readerId") Long readerId);
 }
